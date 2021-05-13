0b94d6698386f6e48f0d86c21b010289d77e2bdb
Owlcuty <taje.chi.i@gmail.com>
2021-05-13 18:36:32 +0300

    to_print

--- parse_cvs.c
+++ parse_cvs.c
@@ -169,12 +169,16 @@ void list_delete_after(s_t* node)
 		return;
 	s_t* cur = node->next;
 	node->next = cur->next;
+	free(cur->fio);
+	free(cur->group);
 	free(cur);
 }
 
 s_t* list_delete_head(s_t* head)
 {
 	s_t* tmp = head->next;
+	free(head->fio);
+	free(head->group);
 	free(head);
 	return tmp;
 }
